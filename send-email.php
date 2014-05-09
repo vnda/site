3d4c37d11eca0c26046ae8f00c0d43501bd4176c
Vagner Oliveira <vagner@vnda.com.br>
2014-05-09 16:42:41 -0300

    Atualização do Conteúdo

--- send-email.php
+++ send-email.php
@@ -1,11 +1,11 @@
 <?php
 if(isset($_POST['email'])) {
-     
+
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "dummy@dummyemail.com";
+    $email_to = "email@vnda.com.br";
     $email_subject = "An email from Envy";
-     
-     
+
+
     function died($error) {
         // your error code can go here
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
@@ -14,21 +14,21 @@ if(isset($_POST['email'])) {
         echo "Please go back and fix these errors.<br /><br />";
         die();
     }
-     
+
     // validation expected data exists
     if(!isset($_POST['name']) ||
         !isset($_POST['email']) ||
         !isset($_POST['message'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+        died('We are sorry, but there appears to be a problem with the form you submitted.');
     }
-     
+
     $name = $_POST['name']; // required
     $email_from = $_POST['email']; // required
     $message = $_POST['message']; // required
-     
+
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-  if(!preg_match($email_exp,$email_from)) {    
+  if(!preg_match($email_exp,$email_from)) {
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
     $string_exp = "/^[A-Za-z .'-]+$/";
@@ -42,30 +42,30 @@ if(isset($_POST['email'])) {
     died($error_message);
   }
     $email_message = "Form details below.\n\n";
-     
+
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
-     
+
     $email_message .= "Name: ".clean_string($name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "Message: ".clean_string($message)."\n";
-      
+
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 
-$mailResponse = mail($email_to, $email_subject, $email_message, $headers);  
+$mailResponse = mail($email_to, $email_subject, $email_message, $headers);
 
 if($mailResponse) { //if $mailResponse is True (if the mail was sent)
 
     header('Location: contact-success.html'); //redirecting to contact-success.html
 
 } else {
-    echo "Sorry. Unable to send the email. Please try again later!";   
+    echo "Sorry. Unable to send the email. Please try again later!";
 }
 
-?> 
+?>
 <?php
 }
 ?>
