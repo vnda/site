3e37de58c781afe04f2275f67058c4dd079a5dc2
Paolla Olsen <paolla@vnda.com.br>
2019-03-25 13:53:44 -0300

    [site-vnda]

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -63,93 +63,95 @@ $(document).ready(function(){
             'Site': $form.find('input[name=site]').val(),
             'Mensagem': $form.find('input[name=mensagem]').val()}
         );
-        $.ajax({
-            url: "https://vnda-73a877.pipedrive.com/v1/organizations?api_token=9fafcfdb813f588d78256ee1d7e105d0735c633c",
-            data: 'name=' + $form.find('input[name=empresa]').val(), 
-            type: 'post',
-            beforeSend: function (){
-                $form.find('button').text('Enviando...');
-            },
-            success: function(result){
-                $.ajax({
-                    url: "https://vnda-73a877.pipedrive.com/v1/persons?api_token=9fafcfdb813f588d78256ee1d7e105d0735c633c",
-                    data: '&name=' + $form.find('input[name=name]').val() + '&email=' + $form.find('input[name=email]').val() + '&phone=' + $form.find('input[name=phone]').val() + '&org_id=' + result.data.id, 
-                    type: 'post',
-                    beforeSend: function (){
-                        $form.find('button').text('Enviando...');
-                    },
-                    success: function(result){
-                        $.ajax({
-                            url: "https://vnda.pipedrive.com/v1/deals?api_token=9fafcfdb813f588d78256ee1d7e105d0735c633c",
-                            data: 'title=' + result.data.name + '&person_id=' + result.data.id + '&org_id=' + result.data.org_id.value, 
-                            type: 'post',
-                            beforeSend: function (){
-                                $form.find('button').text('Enviando...');
-                            },
-                            success: function(result){
-                                $.ajax({
-                                    url: "https://vnda.pipedrive.com/v1/notes?api_token=9fafcfdb813f588d78256ee1d7e105d0735c633c",
-                                    data: 'content=' + $form.find('textarea[name=mensagem]').val() + ' ' + $form.find('input[name=site]').val() + '&deal_id=' + result.data.id, 
-                                    type: 'post',
-                                    beforeSend: function (){
-                                        $form.find('button').text('Enviando...');
-                                    },
-                                    success: function(result){
-                                        console.log('success');
-                                        console.log(result);
-                                        $form.find('button').text('Enviado!');
-                                        $form.find('input[name=name]').val("");
-                                        $form.find('input[name=phone]').val("");
-                                        $form.find('input[name=email]').val("");
-                                        $form.find('input[name=cd6b63656390d27b71205a07369a4040c6025732]').val("");
-                                        $form.find('input[name=e06c1903d6581284343d0561afa2974260f638b3]').val("");
-                                        $form.find('textarea[name=38ed3c827dccbf42ed7b1fe6ebddcaeed3df9c95]').val("");
-                                        setTimeout(function(){
-                                            $form.find('button').text('Enviar!');
-                                        }, 3000);
-                                        
-                                    }, 
-                                    error: function(error){
-                                        console.log('error');
-                                        console.log(error);
-                                        $form.find('button').text('Erro! Tente novamente.');
-                                    },
-                                    complete: function(){
-                                        console.log('Complete');
-                                    }
-                                });
-                            }, 
-                            error: function(error){
-                                console.log('error');
-                                console.log(error);
-                                $form.find('button').text('Erro! Tente novamente.');
-                            },
-                            complete: function(){
-                                console.log('Complete');
-                            }
-                        });
-                    }, 
-                    error: function(error){
-                        console.log('error');
-                        console.log(error);
-                        $form.find('button').text('Erro! Tente novamente.');
-                    },
-                    complete: function(){
-                        console.log('Complete');
-                    }
-                });
-            }, 
-            error: function(error){
-                console.log('error');
-                console.log(error);
-                $form.find('button').text('Erro! Tente novamente.');
-            },
-            complete: function(){
-                console.log('Complete');
-            }
-        });
-
-        
+        if (!$form.hasClass('enviando')){
+            $.ajax({
+                url: "https://vnda-73a877.pipedrive.com/v1/organizations?api_token=9fafcfdb813f588d78256ee1d7e105d0735c633c",
+                data: 'name=' + $form.find('input[name=empresa]').val(), 
+                type: 'post',
+                beforeSend: function (){
+                    $form.addClass('enviando');
+                    $form.find('button').text('Enviando...');
+                },
+                success: function(result){
+                    $.ajax({
+                        url: "https://vnda-73a877.pipedrive.com/v1/persons?api_token=9fafcfdb813f588d78256ee1d7e105d0735c633c",
+                        data: '&name=' + $form.find('input[name=name]').val() + '&email=' + $form.find('input[name=email]').val() + '&phone=' + $form.find('input[name=phone]').val() + '&org_id=' + result.data.id, 
+                        type: 'post',
+                        beforeSend: function (){
+                            $form.find('button').text('Enviando...');
+                        },
+                        success: function(result){
+                            $.ajax({
+                                url: "https://vnda.pipedrive.com/v1/deals?api_token=9fafcfdb813f588d78256ee1d7e105d0735c633c",
+                                data: 'title=' + result.data.name + '&person_id=' + result.data.id + '&org_id=' + result.data.org_id.value, 
+                                type: 'post',
+                                beforeSend: function (){
+                                    $form.find('button').text('Enviando...');
+                                },
+                                success: function(result){
+                                    $.ajax({
+                                        url: "https://vnda.pipedrive.com/v1/notes?api_token=9fafcfdb813f588d78256ee1d7e105d0735c633c",
+                                        data: 'content=' + $form.find('textarea[name=mensagem]').val() + ' ' + $form.find('input[name=site]').val() + '&deal_id=' + result.data.id, 
+                                        type: 'post',
+                                        beforeSend: function (){
+                                            $form.find('button').text('Enviando...');
+                                        },
+                                        success: function(result){
+                                            console.log('success');
+                                            console.log(result);
+                                            $form.find('button').text('Enviado!');
+                                            $form.find('input[name=name]').val("");
+                                            $form.find('input[name=phone]').val("");
+                                            $form.find('input[name=email]').val("");
+                                            $form.find('input[name=cd6b63656390d27b71205a07369a4040c6025732]').val("");
+                                            $form.find('input[name=e06c1903d6581284343d0561afa2974260f638b3]').val("");
+                                            $form.find('textarea[name=38ed3c827dccbf42ed7b1fe6ebddcaeed3df9c95]').val("");
+                                            setTimeout(function(){
+                                                $form.find('button').text('Enviar!');
+                                            }, 3000);
+                                            
+                                        }, 
+                                        error: function(error){
+                                            console.log('error');
+                                            console.log(error);
+                                            $form.find('button').text('Erro! Tente novamente.');
+                                        },
+                                        complete: function(){
+                                            console.log('Complete');
+                                        }
+                                    });
+                                }, 
+                                error: function(error){
+                                    console.log('error');
+                                    console.log(error);
+                                    $form.find('button').text('Erro! Tente novamente.');
+                                },
+                                complete: function(){
+                                    console.log('Complete');
+                                }
+                            });
+                        }, 
+                        error: function(error){
+                            console.log('error');
+                            console.log(error);
+                            $form.find('button').text('Erro! Tente novamente.');
+                        },
+                        complete: function(){
+                            console.log('Complete');
+                        }
+                    });
+                }, 
+                error: function(error){
+                    console.log('error');
+                    console.log(error);
+                    $form.find('button').text('Erro! Tente novamente.');
+                },
+                complete: function(){
+                    console.log('Complete');
+                    $form.removeClass('enviando');
+                }
+            });
+        }
     });
     var swiper = new Swiper($('.slider-cases'), {
         slidesPerView: 1,
